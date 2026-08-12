0e6397e46a12c6d1b140ca7aca36b2a54ad9559f
Dipankar Pal <dipankar.pal@capitalnumbers.com>
2026-08-12 12:37:34 +0530

    wp removed

--- wp-content/themes/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<main class="main-content py-3">
-  <div class="container">
-    <section class="content">
-      <?php get_template_part('loops/404'); ?>
-    </section>
-  </div>
-</main>
-<?php get_footer(); ?>
